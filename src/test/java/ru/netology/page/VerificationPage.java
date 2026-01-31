a0aef7eab7c2b849446140553fc7e87cf4d226b8
Beatific <1telpenkov1@mail.ru>
2026-01-31 09:58:12 +0300

    main

--- src/test/java/ru/netology/page/VerificationPage.java
+++ src/test/java/ru/netology/page/VerificationPage.java
@@ -9,7 +9,6 @@ import static com.codeborne.selenide.Selenide.$;
 public class VerificationPage {
 
     private final SelenideElement codeField = $("[data-test-id='code'] input");
-    private final SelenideElement verifyButton = $("[data-test-id='action-verify']");
     private final SelenideElement heading = $("[data-test-id='code']");
 
     public void shouldBeOnVerificationPage() {
